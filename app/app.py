9cf2f5e6733cc1fda87ebcced9b2c794e15f10c8
sveinnoli <46562494+sveinnoli@users.noreply.github.com>
2021-12-01 10:41:02 +0000

    writes data in json format

--- app/app.py
+++ app/app.py
@@ -1,27 +1,23 @@
 from flask import Flask, request, render_template, redirect
 from modules.db_data import DbData
 
-db = DbData(data_file="text.txt")
+db = DbData(data_file="data.json")
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template("test.html")
 
+@app.route('/scoreboard')
+def scoreboard():
+    return render_template('scoreboard.html', recent_data=db.get_data(3))
+
 @app.route("/scoreboard_post", methods=["POST"])
 def scoreboard_post():
     if request.method == 'POST':
         request_data = request.get_json()
         db.write_data(request_data)
-        return "Success"
-
-@app.route('/demo')
-def demo():
-    return render_template("test.html", response={"response":db.get_data()})
-
-@app.route('/test')
-def test():
-    return render_template("scoreboard.html")
+        return 'Success'
 
 @app.errorhandler(405)
 def err(e):
--- app/modules/db_data.py
+++ app/modules/db_data.py
@@ -1,4 +1,6 @@
+from json.decoder import JSONDecodeError
 from pathlib import Path
+import json
 
 class DbData:
     def __init__(self, data_file):
@@ -6,9 +8,16 @@ class DbData:
         self.data_path = Path(__file__).resolve().parent.parent.joinpath(f'data/{data_file}')
     
     def write_data(self, data):
-        with open(self.data_path, "a") as f:
-            f.writelines(f"{data}\n")
+        with open(self.data_path, "r+") as f:
+            try: 
+                current_data = json.load(f)
+                current_data.append(data)
+                with open(self.data_path, "w") as f2:
+                    json.dump(current_data, f2)
+            except JSONDecodeError:
+                json.dump([data], f)
 
-    def get_data(self):
+
+    def get_data(self, line_quantity=None):
         with open(self.data_path, "r") as f:
-            return f.readlines()
+            return json.load(f)
