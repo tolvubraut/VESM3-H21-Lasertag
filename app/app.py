791aed0a2b9fde8c3df71e79188a2611aa8a18b1
sveinnoli <46562494+sveinnoli@users.noreply.github.com>
2021-12-02 15:26:44 +0000

    updating procfile

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, redirect, url_for
 from modules.db_data import DbData
+import os
 
 db = DbData(data_file="data.json")
 app = Flask(__name__)
@@ -24,4 +25,4 @@ def err(e):
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port = os.environ.get('PORT', 8080))
