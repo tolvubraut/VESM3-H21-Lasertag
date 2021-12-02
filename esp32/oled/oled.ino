1d6d74d3367b86af329758cbcc389c42adbfe2c7
sveinnoli <46562494+sveinnoli@users.noreply.github.com>
2021-12-02 15:03:04 +0000

    updated procf

--- esp32/oled/oled.ino
+++ esp32/oled/oled.ino
@@ -1,10 +1,20 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-void setup() {
-  // put your setup code here, to run once:
-
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define OLED_RESET    -1
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+void setup()   
+{                
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,20);
+  display.println("<yourname>");
+  display.display();
+  display.clearDisplay();
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+void loop() {}
