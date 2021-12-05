2e087a5bce52d1036448fb1c2874cde0abf6dba9
Sveinn oli Gudmundsson <sveinnoli@hotmail.com>
2021-12-05 00:41:21 +0000

    changed url on demo post_request

--- modules/demos/send_post_request.py
+++ modules/demos/send_post_request.py
@@ -1,7 +1,7 @@
 import requests
 from random import randint
 
-print(requests.post(url="http://127.0.0.1:5000/scoreboard_post",
+print(requests.post(url="https://flask-scoreboard.herokuapp.com/scoreboard_post",
             json={
                 "gameid" : randint(1,10),
                 "playerX" : {
